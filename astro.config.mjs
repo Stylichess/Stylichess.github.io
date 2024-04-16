538269ac572958f57e18fd2fbc6a83e11396c134
Paweł Gawlik <pgawlik215@gmail.com>
2024-04-16 23:03:26 +0200

    move assets to public

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://stylichess.github.io",
-  base: "/dist",
   integrations: [tailwind(), react()],
 });
