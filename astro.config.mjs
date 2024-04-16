f5267f1a5107e8b81fa471148427ad78d30a12b3
Paweł Gawlik <pgawlik215@gmail.com>
2024-04-16 22:50:01 +0200

    fix release

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Stylichess.github.io",
+  site: "https://stylichess.github.io",
+  base: "/dist",
   integrations: [tailwind(), react()],
 });
